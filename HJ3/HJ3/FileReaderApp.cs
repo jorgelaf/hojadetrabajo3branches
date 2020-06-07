46af0eba512d279262ccd6b3e622c83cfdb582fa
Jorge Lafuente <jlafont210@gmail.com>
2020-06-07 08:19:59 -0600

    sin patrones

---
+++ HJ3/HJ3/FileReaderApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HJ3
+{
+    class FileReaderApp
+    {
+        static void Main(string[] args)
+        {
+            FileReaderFacade fac = new FileReaderFacade();
+            fac.readFile(args);
+        }
+    }
+}
---
+++ HJ3/HJ3/FileReaderFacade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HJ3
+{
+    class FileReaderFacade
+    {
+        public FileReaderFacade()
+        {
+        }
+
+        public void readFile(string[] files)
+        {
+            for (int i = 0; i < files.Length; i++)
+            {
+                string path = files[i];
+
+                List<string> lineas = new List<string>();
+                lineas = File.ReadAllLines(path).ToList();
+
+                foreach (String linea in lineas)
+                {
+                    string linea2 = linea.Replace(' ', ',');
+                    Console.WriteLine(linea2);
+                }
+            }
+        }
+    }
+}
